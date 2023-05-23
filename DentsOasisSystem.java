2f95576e37c181806e7d16e22e1db67858c9aab6
Lama <131875343+Lamaalbakri@users.noreply.github.com>
2023-05-23 23:09:51 +0300

    Update DentsOasisSystem.java

--- DentsOasisSystem.java
+++ DentsOasisSystem.java
@@ -9,141 +9,141 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class DentsOasisSystem
+
+public class dentsOasis
 {
-    public static void main(String[] args)
+
+    public static void main(String[] args) throws IOException
     {
                 Scanner scanner = new Scanner(System.in);
-                List<Dentist> dentists = new ArrayList<>();
-                List<Patient> patients = new ArrayList<>();
-        
-        
-                System.out.println("********* Welcome to the DentsOasis *********");
+                
+                ArrayList<Dentist> dentists = new ArrayList<>();
+
+                Dentist dentist1 = new Dentist("Layan", "0548402813", "layan@gmail.com" , "Makkah", "Orthodontist", "10 years", "phd");
+                dentists.add(dentist1);
+                Dentist dentist2 = new Dentist("Lama", "0547392023", "lama@gmail.com" , "Jeddah", "Periodontist", "7years", "master");
+                dentists.add(dentist2);
+                Dentist dentist3 = new Dentist("Raghad", "0585729224", "raghad@gmail.com" , "Riyadh", "Endodontist", "5 years", "bachelor");
+                dentists.add(dentist3);
+                Dentist dentist4 = new Dentist("Mohammed", "0543839112", "moha@gmail.com" , "Makkah", "Orthodontist", "20 years", "phd");
+                dentists.add(dentist4);
+                
+                
+                Appointment appointment1 = new Appointment(1, "10:00 AM", "2023-05-01");
+                Appointment appointment2 = new Appointment(2, "11:00 AM", "2023-05-01");
+                dentist1.addAppointment(appointment1);
+                dentist1.addAppointment(appointment2);     
+
+                
+                
+                Appointment appointment3 = new Appointment("10:00 AM", "2023-05-01");
+                Appointment appointment4 = new Appointment("11:00 AM", "2023-05-01");
+                dentist4.addAppointment(appointment3);
+                dentist4.addAppointment(appointment4);     
+                
+                
+                Appointment appointment5 = new Appointment("10:00 AM", "2023-05-01");
+                Appointment appointment6 = new Appointment("11:00 AM", "2023-05-01");
+                dentist3.addAppointment(appointment5);
+                dentist3.addAppointment(appointment6);     
+                
+                
+                Appointment appointment7 = new Appointment("10:00 AM", "2023-05-01");
+                Appointment appointment8 = new Appointment("11:00 AM", "2023-05-01");
+                dentist2.addAppointment(appointment7);
+                dentist2.addAppointment(appointment8);   
+
+           
+                System.out.println("\n\t********* Welcome to the DentsOasis *********");
                 System.out.println();
-                System.out.println("Dentist or patient?");
-                System.out.println("1. Dentist");
-                System.out.println("2. Patient");
-                System.out.println("3. Exit");
-                int input = scanner.nextInt();
-
-                switch (input) {
-                    case 1:
-                        System.out.println("\n*** DENTIST PAGE ***");
+                
+                    	System.out.println("\n    *** PATIENT PAGE ***");
                         System.out.println("*** Choose the following ***");
                         System.out.println("1. Login");
                         System.out.println("2. Register");
                         System.out.println("3. Exit");
-                        
-                        int select = scanner.nextInt();
-                        scanner.nextLine();
-                        switch (select) {
-                            case 1:
-                               if(loginDentist()!=true)
-                                   System.out.println("not found");
-                               else {
-                                System.out.println("\n*** DENTIST PAGE ***");
-                                System.out.println("*** Choose the following ***");
-                                System.out.println("1. Edit information");
-                                System.out.println("2. View appointments");
-                                System.out.println("3. Exit");
-
-                                int choice = scanner.nextInt();
-                                scanner.nextLine();
-
-                                switch (choice) {
-                                    case 1:
-                                        System.out.println("1. Edit Dentist Details");
-
-                                        System.out.print("Enter specialty: ");
-                                        String specialty = scanner.nextLine();
-                                        
-                                        System.out.print("Enter Qualifications: ");
-                                        String Qualifications = scanner.nextLine();
-                                        
-                                        System.out.print("Enter Experience: ");
-                                        String Experience = scanner.nextLine();
-                                       
-                                        System.out.print("Enter city: ");
-                                        String city = scanner.nextLine();
-                                       
-                                        
-                                        // Create a new Doctor object and add it to the ArrayList
-                                        Dentist dentist = new Dentist(specialty,Qualifications, Experience, city);
-                                        dentists.add(dentist);
-
-                                    case 3:
-                                        break;
-
-                                    default:
-                                        System.out.println("Try again");
-                                }
-                                
-                            }                               
-                                break;
-                            case 2:
-                            	registerDentist();
-                                break;
-                            case 3:
-                                System.exit(0);
-                                break;
-                            default:
-                                System.out.println("Try again");
-                                break;     
-                        }
-                        break;
-                    case 2:
-                    	System.out.println("\n*** PATIENT PAGE ***");
-                        System.out.println("*** Choose the following ***");
-                        System.out.println("1. Login");
-                        System.out.println("2. Register");
-                        System.out.println("3. Exit");
+                        System.out.print(">> Enter your choice: ");
+                        int select;
                         select = scanner.nextInt();
                         scanner.nextLine();
+                        
                         switch (select) {
                             case 1:
                                 if(loginPatient()!=true) 
                                     System.out.println("not found");
 
-                                else {
-
-                                System.out.println("\n*** PATIENT MENU ***");
-                                System.out.println("1. Search for Dentist");
-                                System.out.println("2. View Dentist Details");
+                                else 
+                                {
+                                    while(true)
+                                    {
+                                System.out.println("\n    *** PATIENT MENU ***");
+                                System.out.println("1. Search for Dentist and Book an Appointment");
+                                System.out.println("2. Cancel appointments");
                                 System.out.println("3. Provide Review for a Dentist");
-                                System.out.println("4. Book an Appointment");
-                                System.out.println("5. Exit");
-                                System.out.print("Enter your choice: ");
+                                System.out.println("4. Exit");
+                                System.out.print(">> Enter your choice: ");
                                 int choice = scanner.nextInt();
                                 scanner.nextLine();
 
-                                switch (choice) {
+                                switch (choice) 
+                                {
                                 case 1:
-                                    System.out.print("Enter city: ");
+                                    
+                                    System.out.print("\nEnter city: ");
                                     String city = scanner.nextLine();
+                                    
                                     System.out.print("Enter specialty: ");
                                     String specialty = scanner.nextLine();
+                                    
+                                    //call search method
+                                    List<Dentist> searchResults = searchDents(city, specialty, dentists);
+                                    
+                                    if (searchResults.isEmpty()) 
+                                    {
+                                        System.out.println("Not found.");
+                                        return;
+                                    }
 
-                                    List<Dentist> searchResults = searchDentist(dentists, city, specialty);
-                                    if (searchResults.size() == 0) {
-                                        System.out.println("No dentists found in " + city + " with " + specialty + " specialization.");
-                                    } else {
-                                        System.out.println("Search Results:");
-                                        System.out.println("ID\tDentist Name\tSpecialty\tQualifications\tExperience");
-                                        for (int i = 0; i < searchResults.size(); i++) {
-                                            Dentist dentist = searchResults.get(i);
-                                            System.out.println(i+1 + "\t" + dentist.getName() + "\t" + dentist.getSpecialty() + "\t" +
-                                                    dentist.getQualification() + "\t" + dentist.getExperience());
-                                        }
+                                    // display the search results
+                                    System.out.print("\n>> Search results:");
+                                    
+                                    for (int i = 0; i < searchResults.size(); i++) {
+                                        System.out.println();
+                                        Dentist dentist = searchResults.get(i);
+                                        System.out.println((i + 1) + ". " + dentist.getName() + " - " + dentist.getSpecialty() + " - " + dentist.getCity());
+                                        
+                                      
+                                    }
+                                    //Ask the user to book an appointment
+                                    System.out.println("\nBook an appointment? Yes or No ");
+                                    String in = scanner.nextLine();
+
+                                    if (in.equalsIgnoreCase("yes")) {
+                                     Appointment.promptUserToChooseAppointment(dentists);
                                     }
                                     break; // add this break statement
-                                   
-                                    default:
+                                case 2:
+                                    System.out.println("Cancel Appointment ");
+
+                                	
+                                	break; 
+                                	
+                                case 3:
+                                    System.out.println("Review Dentist ");
+
+                                	break;
+    
+                                case 4:
+                                    System.exit(0);
+                                    break;
+                                default:
                                         System.out.println("Try again");
                                 }
-                            }
+                            } // end while
+                                } // end else
                                 break;
                             case 2:
-                            	patients.add(registerPatient());
+                            	registerPatient();
                                 break;
                             case 3:
                                 System.exit(0);
@@ -152,23 +152,26 @@ public class DentsOasisSystem
                                 System.out.println("Try again");
                                 break;
                         
-                        }
-                }
+                        
+                                }
+    
+                }// end main
                 
                 
-}
 
-    public static List<Dentist> searchDentist(List<Dentist> dentists, String city, String specialty) {
-        List<Dentist> results = new ArrayList<>();
-                
-        for (Dentist dentist : dentists) {
-            if (dentist.getCity().equals(city) && dentist.getSpecialty().equals(specialty)) {
-                results.add(dentist);
-            }
-        }
-        return results;
+    public static ArrayList<Dentist> searchDents(String city, String specialty, ArrayList<Dentist> dentists) throws IOException {
+    	ArrayList<Dentist> results = new ArrayList<>();
+
+    	for (Dentist dentist : dentists) {
+    	    if (dentist.getCity().equalsIgnoreCase(city) && dentist.getSpecialty().equalsIgnoreCase(specialty)) {
+    	        results.add(dentist);
+    	    }
+    	}
+
+    	return results;
+       
     }
-
+    
     public static boolean loginPatient() {
           Scanner keyboard = new Scanner(System.in);
  		  Patient patient = new Patient();
@@ -196,106 +199,7 @@ public class DentsOasisSystem
     	return false;
     	}
      
-       public static boolean loginDentist() {
-           Scanner keyboard = new Scanner(System.in);
-		   Dentist dentist = new Dentist();
-
-    	   System.out.println("\n**** Login for Dentist **** ");
-		   System.out.println("Enter Username :");
-		    dentist.setUsername(keyboard.nextLine()); 
-		   System.out.println("Enter Password : ");
-		    dentist.setPassword(keyboard.nextLine()); 
        
-        	try {
-        	Scanner scan = new Scanner(new FileReader("dentist.txt"));
-        	while (scan.hasNextLine()) {
-        	String line = scan.nextLine();
-        	String[] details = line.split(",");
-        	if (details[0].equals(dentist.getUsername()) && details[1].equals(dentist.getPassword())) {
-        	return true;
-        	
-        } 
-       }
-      }
-        	catch (FileNotFoundException e) {
-        	e.printStackTrace();
-        	}
-        	
-        	
-        	return false;
-       }
-       
-       public static void registerDentist()  {
-     	  Scanner keyboard = new Scanner(System.in);
-          Dentist dentist = new Dentist();
-          
-          
-     	   System.out.println("\n**** Register for Dentist **** ");
-     	   
-     	   
-     	       System.out.println("Enter your username : ");
-     	          dentist.setUsername(keyboard.nextLine()); 
-			   if (isValidUsername(dentist.getUsername()) != true) {
-				 System.out.println("Please Try again ");
-	 			   System.out.println("Enter valid username : ");
-	 			  dentist.setUsername(keyboard.nextLine()); 
-			   }
-			   System.out.println("Enter your password : ");
-			   dentist.setPassword(keyboard.nextLine()); 
-			   
-			   
-			 if (isValidPassword(dentist.getPassword()) != true) {
-				 System.out.println("Please Try again ");
-	 			 System.out.println("Should contains numbers, special character,  and length between 8 to 10 ");
-	 			   System.out.println("Enter valid password : ");
-	 			  dentist.setPassword(keyboard.nextLine()); 
-			   }
-			 
-			   System.out.println("Enter your mobile : ");
-			   dentist.setPhoneNumber(keyboard.nextLine()); 
-			   
-			 if (isValidPhoneeNumber(dentist.getPhoneNumber()) != true) {
-				 System.out.println("Please Try again ");
-	 			   System.out.println("Enter valid phone number : ");
-	 			  dentist.setPhoneNumber(keyboard.nextLine()); 
-			   }
-			   System.out.println("Enter your email : ");
-			   dentist.setEmail(keyboard.nextLine()); 
-			 if (isValidEmail(dentist.getEmail()) != true) {
-				 System.out.println("Please Try again ");
-	 			   System.out.println("Enter valid email : ");
-	 			  dentist.setEmail(keyboard.nextLine()); 
-			   }
-			   
-			   System.out.println("Enter your City : ");
-			   dentist.setCity(keyboard.nextLine()); 
-			   
-			   System.out.println("Enter your Specialty : ");
-			   dentist.setSpecialty(keyboard.nextLine()); 
-			   
-			   System.out.println("Enter your Qualifications : ");
-			   dentist.setDentistQualification(keyboard.nextLine()); 
- 		  
-			   System.out.println("Enter your Experience : ");
-			   dentist.setExperience(keyboard.nextLine()); 
- 		  
-				
-				
- 		  try {
-        	   FileWriter write = new FileWriter("dentist.txt");
-		    	PrintWriter print = new PrintWriter(write);
-		    	print.print(dentist.getUsername()+ "," +  dentist.getPassword() + "," + dentist.getPhoneNumber()
-	             +"," + dentist.getEmail() + "," + dentist.getCity()+ "," + dentist.getSpecialty() + "," + dentist.getQualification()+ "," + dentist.getExperience() +  ",\n");
-	 			   System.out.println(" Successful registration ");
-
-		    	print.close();
-           }
-			 catch (IOException e) {
-				e.printStackTrace();
-			}
-      
-       }
-          
        public static Patient registerPatient()  {   
       	      Scanner keyboard = new Scanner(System.in);
      		  Patient patient = new Patient();
@@ -343,7 +247,7 @@ public class DentsOasisSystem
  	 			   patient.setCity(keyboard.nextLine()); 
  			 
  	           try {
- 	        	   FileWriter write = new FileWriter("patient.txt");
+ 	        	   FileWriter write = new FileWriter("patient.txt", true);
  			    	PrintWriter print = new PrintWriter(write);
  			    	print.print(patient.getUsername()+ "," +  patient.getPassword() + "," + patient.getPhoneNumber()
  		             +"," + patient.getEmail() + "," + patient.getCity()+ ",\n");
