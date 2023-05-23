d1db2328f0642f8508a0cbbfcfcf9a7d8e4625ba
Lama <131875343+Lamaalbakri@users.noreply.github.com>
2023-05-23 23:06:26 +0300

    Update Dentist.java

--- Dentist.java
+++ Dentist.java
@@ -1,53 +1,93 @@
 package dentsOasis;
 
-public class Dentist extends User
-{
-    private String dentistSpecialty;
-    private String dentistExperience;
-    private String dentistQualification;
-    
-    public Dentist(){
-    
+import java.util.ArrayList;
+
+public class Dentist {
+	private String name;
+	private String phoneNumber;
+	private String email;
+	private String city;
+	private String dentistSpecialty;
+	private String dentistExperience;
+	private String dentistQualification;
+    private ArrayList<Appointment> appointments;
+	
+	
+    public Dentist( ) {
+		
+		
+	}
+  
+	public Dentist (String name, String phoneNumber, String email, String city, String Specialty,
+                String experience, String qualification) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.city = city;
+		dentistSpecialty = Specialty;
+		dentistExperience  = experience;
+		dentistQualification= qualification;
+        this.appointments = new ArrayList<>();
+
+	}
+    public ArrayList<Appointment> getAppointments() {
+        return appointments;
     }
-    public Dentist(String Specialty,String qualification, String experience, String city){
-        
-        super(city);
-	dentistSpecialty = Specialty;
-	dentistQualification = qualification;
-	dentistExperience = experience;
+
+    public void addAppointment(Appointment appointment) {
+        appointments.add(appointment);
     }
-    public Dentist(String Specialty,String experience, String qualification, String name, String phoneNumber,
-            String email, String username, String password, String city){
-        
-        super(name, phoneNumber, email, username,  password,  city);
-	dentistSpecialty = Specialty;
-	dentistExperience  = experience;
-	dentistQualification= qualification;
-    }
-    
-    public String getSpecialty(){
-        return dentistSpecialty;
-    }
-    public void setSpecialty(String dentistSpecialty ){
-        this.dentistSpecialty = dentistSpecialty;
-    }
-    public String getExperience(){
-        return dentistExperience;
-    }
-    public void setExperience(String experience){
-        this.dentistExperience = experience;
-    }
-    public String getQualification(){
-        return dentistQualification;
-    }
-    public void setDentistQualification(String dentistQualification){
-        this.dentistQualification = dentistQualification;
-    }
-    
-    public void createProfile(){
-        
-    }
-    public void editProfile(){
-        
-    }	
+
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getSpecialty() {
+		return dentistSpecialty;
+	}
+	public void setSpecialty(String dentistSpecialty ) {
+		this.dentistSpecialty = dentistSpecialty;
+	}
+	
+	public String getExperience() {
+		return dentistExperience;
+	}
+	public void setExperience(String experience) {
+		this.dentistExperience = experience;
+	}
+	public String getQualification() {
+		return dentistQualification;
+	}
+	public void setDentistQualification(String dentistQualification) {
+		this.dentistQualification = dentistQualification;
+	}
+	public void createProfile() {
+	}
+	
+	public void editProfile() {
+	}	
 }
