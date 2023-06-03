ead4cb8c999933b99abf4bb54027e7ed192cb417
Layan <80457132+Layanalahmadi@users.noreply.github.com>
2023-06-03 13:00:23 +0300

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,7 +1,6 @@
-package dentsOasis;
-
-public class Patient {
-	private String name;
+public class Patient
+{
+        private String name;
 	private String phoneNumber;
 	private String email;
 	private String username;
@@ -12,52 +11,51 @@ public class Patient {
 	public Patient () {
 		
 	}
-	
-	public Patient (String name, String phoneNumber, String email,  String username, String password, String City) {
-		 
-		this.name = name;
-		this.phoneNumber = phoneNumber;
-		this.email = email;
-		this.username = username;
-		this.password = password;
-		this.city = city;
-		
+	public Patient(String name) {
+            this.name = name;
+        }
+	public Patient (String name, String phoneNumber, String email,  String username, String password, String City){ 
+        this.name = name;
+	    this.phoneNumber = phoneNumber;
+	    this.email = email;
+	    this.username = username;
+	    this.password = password;
+	    this.city = city;
 	}
-	public String getName() {
-		return name;
+	public String getName(){
+            return name;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setName(String name){
+            this.name = name;
 	}
-	
-	public String getPhoneNumber() {
-		return phoneNumber;
+	public String getPhoneNumber(){
+            return phoneNumber;
 	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
+	public void setPhoneNumber(String phoneNumber){
+            this.phoneNumber = phoneNumber;
 	}
-	public String getEmail() {
-		return email;
+	public String getEmail(){
+            return email;
 	}
-	public void setEmail(String email) {
-		this.email = email;
+	public void setEmail(String email){
+            this.email = email;
 	}
-	public String getUsername() {
-		return username;
+	public String getUsername(){
+            return username;
 	}
-	public void setUsername(String username) {
-		this.username = username;
+	public void setUsername(String username){
+            this.username = username;
 	}
-	public String getPassword() {
-		return password;
+	public String getPassword(){
+            return password;
 	}
-	public void setPassword(String password) {
-		this.password = password;
+	public void setPassword(String password){
+            this.password = password;
 	}
-	public String getCity() {
-		return city;
+	public String getCity(){
+            return city;
 	}
 	public void setCity(String city) {
-		this.city = city;
+            this.city = city;
 	}
 }
